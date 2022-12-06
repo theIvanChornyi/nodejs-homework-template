086f68f395dfb60cf53f527507f04c2a28c8b6d3
Ivan Chornyi <107806721+theIvanChornyi@users.noreply.github.com>
2022-12-06 14:33:04 +0200

    add REST API methods

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,97 @@
-// const fs = require('fs/promises')
+const fs = require('fs').promises;
+const path = require('path');
+const ObjectId = require('bson-objectid');
 
-const listContacts = async () => {}
+const contacts = path.join(__dirname, './contacts.json');
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contacts, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    return errorFunction(error);
+  }
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async contactId => {
+  try {
+    const data = await listContacts();
+    const [contact] = await data.filter(({ id }) => id === contactId);
+    return contact;
+  } catch (error) {
+    return errorFunction(error);
+  }
+};
 
-const addContact = async (body) => {}
+const removeContact = async contactId => {
+  try {
+    const data = await listContacts();
+    const indexOfDeleteContact = [...data].reduce((acc, contact, index) => {
+      if (contact.id === contactId) {
+        acc = index;
+      }
+      return acc;
+    }, -1);
 
-const updateContact = async (contactId, body) => {}
+    if (indexOfDeleteContact !== -1) {
+      data.splice(indexOfDeleteContact, 1);
+      const normalizedContacts = JSON.stringify(data);
+      await fs.writeFile(contacts, normalizedContacts, null, 2, 'utf8');
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    return errorFunction(error);
+  }
+};
+
+const addContact = async body => {
+  try {
+    const contactList = await listContacts();
+    const newContact = { id: ObjectId(), ...body };
+    const newContactList = [...contactList, newContact];
+    const normalizedNewContactList = JSON.stringify(newContactList);
+    await fs.writeFile(contacts, normalizedNewContactList, null, 2, 'utf8');
+    return newContactList;
+  } catch (error) {
+    return errorFunction(error);
+  }
+};
+
+const updateContact = async (contactId, body) => {
+  try {
+    const data = await listContacts();
+
+    const indexOfDeleteContact = [...data].reduce((acc, contact, index) => {
+      if (contact.id === contactId) {
+        acc = index;
+      }
+      return acc;
+    }, -1);
+
+    if (indexOfDeleteContact !== -1) {
+      data.splice(indexOfDeleteContact, 1, {
+        ...data[indexOfDeleteContact],
+        ...body,
+      });
+
+      const normalizedContacts = JSON.stringify(data);
+      await fs.writeFile(contacts, normalizedContacts, null, 2, 'utf8');
+
+      return data;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    return errorFunction(error);
+  }
+};
+
+function errorFunction(error) {
+  console.log(error.message);
+  return null;
+}
 
 module.exports = {
   listContacts,
@@ -16,4 +99,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
